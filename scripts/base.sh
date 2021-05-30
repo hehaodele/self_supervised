7dcc60d7ba4722c7f421fac709d26438f37d0091
hehaodele <haohe@mit.edu>
2021-05-30 02:30:52 -0400

    base

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBILE_DEVICES=0 python entry.py -m moco &
-CUDA_VISIBILE_DEVICES=1 python entry.py -m byol &
-CUDA_VISIBILE_DEVICES=2 python entry.py -m simclr &
-CUDA_VISIBILE_DEVICES=3 python entry.py -m eqco &
+CUDA_VISIBLE_DEVICES=0 python entry.py -m moco &
+CUDA_VISIBLE_DEVICES=1 python entry.py -m byol &
+CUDA_VISIBLE_DEVICES=2 python entry.py -m simclr &
+CUDA_VISIBLE_DEVICES=3 python entry.py -m eqco &
