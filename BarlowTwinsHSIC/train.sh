4549994aad0f709adc7635c1947834b0161f889a
hehaodele <haohe@mit.edu>
2021-05-30 15:55:41 -0400

    base

---
+++ BarlowTwinsHSIC/train.sh
@@ -0,0 +1,2 @@
+python main.py --lmbda 0.0078125 --corr_zero --batch_size 128 --feature_dim 128 --dataset stl10
+python linear.py --dataset stl10 --model_path results/0.0078125_128_128_stl10_model.pth
