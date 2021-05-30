bee2bb56b2ad2bb0b9c2a900babd5e3048574846
hehaodele <haohe@mit.edu>
2021-05-30 16:21:04 -0400

    base

---
+++ scripts/various_p.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python entry.py -m moco   -d stl10-id --id-type 2d --id-weight 0.5 &
+CUDA_VISIBLE_DEVICES=1 python entry.py -m byol   -d stl10-id --id-type 2d --id-weight 0.5 &
+CUDA_VISIBLE_DEVICES=2 python entry.py -m simclr -d stl10-id --id-type 2d --id-weight 0.5 &
+CUDA_VISIBLE_DEVICES=3 python entry.py -m eqco   -d stl10-id --id-type 2d --id-weight 0.5 &
