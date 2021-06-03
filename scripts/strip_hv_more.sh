9b3effd83c3f9c1eeb6d775714ea7a0e88c7b52a
hehaodele <haohe@mit.edu>
2021-06-03 11:28:34 -0400

    stamp learning

--- scripts/strip_hv_more.sh
+++ scripts/strip_hv_more.sh
@@ -6,7 +6,11 @@
 #CUDA_VISIBLE_DEVICES=2 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 32 &
 #CUDA_VISIBLE_DEVICES=0 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 1.0 --strip-len 8 &
 #CUDA_VISIBLE_DEVICES=3 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 1.0 --strip-len 64 &
-CUDA_VISIBLE_DEVICES=1 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.75 --strip-len 8 &
-CUDA_VISIBLE_DEVICES=2 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 8 &
-CUDA_VISIBLE_DEVICES=0 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.75 --strip-len 64 &
-CUDA_VISIBLE_DEVICES=3 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 64 &
+#CUDA_VISIBLE_DEVICES=1 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.75 --strip-len 8 &
+#CUDA_VISIBLE_DEVICES=2 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 8 &
+#CUDA_VISIBLE_DEVICES=0 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.75 --strip-len 64 &
+#CUDA_VISIBLE_DEVICES=3 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 64 &
+CUDA_VISIBLE_DEVICES=1 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.75 --strip-len 4 &
+CUDA_VISIBLE_DEVICES=2 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 0.25 --strip-len 4 &
+CUDA_VISIBLE_DEVICES=0 python entry.py -m moco   -d stl10-id --id-type strip-hv --id-weight 1.0 --strip-len 4 &
+CUDA_VISIBLE_DEVICES=3 python entry.py -m moco   -d stl10-id --id-type strip --id-weight 0.5 --strip-len 4 &
